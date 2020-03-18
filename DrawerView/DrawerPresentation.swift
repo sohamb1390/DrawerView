fc748b9dad38707ede3138e27a02478d355fcb49
Soham Bhattacharjee <sohamb.1390@gmail.com>
2020-03-18 11:07:20 +0530

    UIKit imported

--- DrawerView/DrawerPresentation.swift
+++ DrawerView/DrawerPresentation.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public protocol DrawerPresenter {
     func presentDrawerModal(_ presentedViewController: UIViewController, openHeightBehavior: DrawerView.OpenHeightBehavior)
